d481e308f62fdf54db1e46706866c6ae58c14972
史亚军 <>
2020-09-07 11:26:11 +0800

    快速排序

---
+++ src/main/kotlin/x/app/alg/AlgApplication.kt
@@ -0,0 +1,11 @@
+package x.app.alg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlgApplication>(*args)
+}
---
+++ src/main/kotlin/x/app/alg/sorts/TopKth.kt
@@ -0,0 +1,8 @@
+package x.app.alg.sorts
+
+/**
+ * @author shiyajun
+ * @date 2020/9/7 10:52 上午
+ * */
+class TopKth {
+}
